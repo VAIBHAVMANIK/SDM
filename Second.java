588f0fdeaf81780c0f08ea7a01223caaed37da1b
VAIBHAVMANIK <81464192+VAIBHAVMANIK@users.noreply.github.com>
2022-12-27 16:04:59 +0530

    Add addition  in second.java

--- Second.java
+++ Second.java
@@ -1,5 +1,7 @@
 public class File {
     public static void main(String[] args) {
-        System.out.println("Hello World!..");
+        int a =10 ;
+        int b = 20 ;
+        System.out.println((a+b));
     }
 }
